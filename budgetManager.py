ce5608b87602c16b08610287761d0b2eb8f272c4
Daniel Della Posta <Danieldellaposta43@gmail.com>
2025-11-27 05:54:59 +0100

    patch 1
    aggiunta del menu più preciso e del date_time

--- budgetManager.py
+++ budgetManager.py
@@ -2,19 +2,24 @@ import pandas as pd
 from datetime import date
 import os
 
-class budgetManager:
-    FILE = "budget-csv"
+class BudgetManager:
+    FILE = "budget.csv"
 
     def __init__(self):
         if not os.path.exists(self.FILE) or os.path.getsize(self.FILE) == 0:
-            self.df = pd.DataFrame(colum=["obejct", "money", "date"])
+            self.df = pd.DataFrame(columns=["object", "money", "date"])
         else:
             self.df = pd.read_csv(self.FILE)
+            self.df["date"] = pd.to_datetime(self.df["date"], errors="coerce")
 
     def add_entry(self, obj, money, date):
-        new_row = {"object": obj, "money": money, "date": date}
-        self.df = pd.concat([self.df, pd.DataFrame([new_row])], ignore_infex=True)
+        new_row = {"object": obj, "money":float(money), "date": pd.to_datetime(date)}
+        self.df = pd.concat([self.df, pd.DataFrame([new_row])], ignore_index=True)
         self.save()
 
     def save(self):
         self.df.to_csv(self.FILE, index=False)
+
+    def delete_all(self):
+        self.df = pd.DataFrame(columns = self.df.columns)
+        self.save()
